41032476a5df7d7f6e3bc65f1f3d1c7db690e0e6
Goutham Sundaram <sundadram18@up.edu>
2016-11-21 23:31:37 -0800

    outline for a.i

--- app/src/main/java/com/example/keokimassad/testparcheesi/ParComputerPlayerE.java
+++ app/src/main/java/com/example/keokimassad/testparcheesi/ParComputerPlayerE.java
@@ -10,6 +10,7 @@ import game.infoMsg.GameInfo;
 public class ParComputerPlayerE extends GameComputerPlayer {
 
     ParLocalGame parLocalGame = new ParLocalGame();
+    int player1 = playerTurn;
     private int[] location = new int[4];
 
     public ParComputerPlayerE (String name) {
@@ -18,7 +19,39 @@ public class ParComputerPlayerE extends GameComputerPlayer {
 
     @Override
     protected void receiveInfo(GameInfo info) {
-        if(parLocalGame)
+        if(parLocalGame.canMove(player1))
+        {
+            if(info instanceof ParGameState)
+            {
+                if //(one or two of pieces are at home)
+                {
+                    final Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            //have one dice be three and one dice be 2
+                            //bring one piece out
+                        }
+                    }, 2000);
 
+                }
+                else
+                {
+                    final Handler handler = new Handler();
+                    handler.postDelayed(new Runnable(){
+                        @Override
+                        public void run() {
+                            //roll two 1's and advance each piece one spot
+                        }
+                    }, 2000);
+
+                }
+            }
+        }
+
+    }
+    else
+    {
+        return;
     }
 }
