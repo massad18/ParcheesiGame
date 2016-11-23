bb0e5728651abfe72c533de16d8e11e217a7e15d
Justin Hathaway <justin_hathaway@live.com>
2016-11-23 00:46:33 -0800

    made temp changes to Human Player to get the dice actually rolling

--- app/src/main/java/com/example/keokimassad/testparcheesi/ParHumanPlayer.java
+++ app/src/main/java/com/example/keokimassad/testparcheesi/ParHumanPlayer.java
@@ -1,6 +1,7 @@
 package com.example.keokimassad.testparcheesi;
 
 import android.graphics.Color;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
@@ -48,8 +49,52 @@ public class ParHumanPlayer extends GameHumanPlayer implements View.OnClickListe
     @Override
     public void receiveInfo(GameInfo info)
     {
+        switch(parState.getDice1Val())
+        {
+            case 1:
+                //diceButtons[0].setImageResource(R.mipmap.die_val_1);
+                diceButtons[0].setImageResource(R.drawable.die1);
+                break;
+            case 2:
+                diceButtons[0].setImageResource(R.drawable.die2);
+                break;
+            case 3:
+                diceButtons[0].setImageResource(R.drawable.die3);
+                break;
+            case 4:
+                diceButtons[0].setImageResource(R.drawable.die4);
+                break;
+            case 5:
+                diceButtons[0].setImageResource(R.drawable.die5);
+                break;
+            case 6:
+                diceButtons[0].setImageResource(R.drawable.die6);
+                break;
+        }
+        switch(parState.getDice2Val())
+        {
+            case 1:
+                //diceButtons[0].setImageResource(R.mipmap.die_val_1);
+                diceButtons[1].setImageResource(R.drawable.die1);
+                break;
+            case 2:
+                diceButtons[1].setImageResource(R.drawable.die2);
+                break;
+            case 3:
+                diceButtons[1].setImageResource(R.drawable.die3);
+                break;
+            case 4:
+                diceButtons[1].setImageResource(R.drawable.die4);
+                break;
+            case 5:
+                diceButtons[1].setImageResource(R.drawable.die5);
+                break;
+            case 6:
+                diceButtons[1].setImageResource(R.drawable.die6);
+                break;
+        }
         //Checks to make sure the game info being sent in is an instance of the state class
-        if(info instanceof ParState)
+        /*if(info instanceof ParState)
         {
             //Changes the image of die 1 based on the current die 1 value
             switch(((ParState)info).getDice1Val())
@@ -101,7 +146,7 @@ public class ParHumanPlayer extends GameHumanPlayer implements View.OnClickListe
         {
             //Screen flashes black in info isn't an instance of ParState (which shouldn't occur unless error)
             flash(Color.BLACK, 5);
-        }
+        }*/
     }
 
     public void onClick (View button) {
@@ -114,6 +159,9 @@ public class ParHumanPlayer extends GameHumanPlayer implements View.OnClickListe
             {
                 //Player has yet to roll the dice
                 case 0:
+                    int die1 = (int)(Math.random()*6) + 1;
+                    int die2 = (int)(Math.random()*6) + 1;
+                    parState.setDieVals(die1,die2);
                     //ParRollAction is called to roll the dice
                     ParRollAction actRoll = new ParRollAction(this);
                     game.sendAction(actRoll);
@@ -121,10 +169,13 @@ public class ParHumanPlayer extends GameHumanPlayer implements View.OnClickListe
                 //Player has rolled and has to move pawns
                 case 1:
                 case 2:
+                    die1 = (int)(Math.random()*6) + 1;
+                    die2 = (int)(Math.random()*6) + 1;
+                    parState.setDieVals(die1,die2);
                     //ParMoveAction is called to move a pawn
                     ParMoveAction actMove = new ParMoveAction(this);
                     game.sendAction(actMove);
-                    diceButtons[0].setEnabled(false); //sets die to no longer be pressed
+                    //diceButtons[0].setEnabled(false); //sets die to no longer be pressed
                     break;
             }
         }
@@ -135,6 +186,10 @@ public class ParHumanPlayer extends GameHumanPlayer implements View.OnClickListe
             {
                 //Player has yet to roll the dice
                 case 0:
+                    System.out.println("Entering case 0");
+                    int die1 = (int)(Math.random()*6) + 1;
+                    int die2 = (int)(Math.random()*6) + 1;
+                    parState.setDieVals(die1,die2);
                     //ParRollAction is called to roll the dice
                     ParRollAction actRoll = new ParRollAction(this);
                     game.sendAction(actRoll);
@@ -142,10 +197,14 @@ public class ParHumanPlayer extends GameHumanPlayer implements View.OnClickListe
                 //Player has rolled and has to move pawns
                 case 1:
                 case 2:
+                    System.out.println("Entering case 0");
+                    die1 = (int)(Math.random()*6) + 1;
+                    die2 = (int)(Math.random()*6) + 1;
+                    parState.setDieVals(die1,die2);
                     //ParMoveAction is called to move a pawn
                     ParMoveAction actMove = new ParMoveAction(this);
                     game.sendAction(actMove);
-                    diceButtons[1].setEnabled(false); //sets die to no longer be pressed
+                    //diceButtons[1].setEnabled(false); //sets die to no longer be pressed
                     break;
             }
         }
@@ -171,6 +230,9 @@ public class ParHumanPlayer extends GameHumanPlayer implements View.OnClickListe
         diceButtons[0].setOnClickListener(this);
         diceButtons[1].setOnClickListener(this);
         touchView.setOnTouchListener(this);
+
+        //ToDo:Not sure if temp
+        parState.setCurrentSubstage(0);
     }
 
     @Override
