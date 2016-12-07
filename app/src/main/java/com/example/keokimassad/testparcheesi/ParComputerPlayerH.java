84a3948507380ef1e4e59fa25867d43752228a21
KeokiMassad <massad18@up.edu>
2016-12-07 14:25:07 -0800

    Tried to mirror the easy comp player to the hard comp player... also added ToDo's in which we need to implement...

--- app/src/main/java/com/example/keokimassad/testparcheesi/ParComputerPlayerH.java
+++ app/src/main/java/com/example/keokimassad/testparcheesi/ParComputerPlayerH.java
@@ -22,6 +22,7 @@ public class ParComputerPlayerH extends GameComputerPlayer {
     ParSelectAction selectAction;
     ParUseDieAction useDieAction;
     ParMoveAction moveAction;
+    ParCheckLegalMoveAction checkLegalMoveAction;
     ParState parState;
 
 
@@ -57,100 +58,131 @@ public class ParComputerPlayerH extends GameComputerPlayer {
         ParState parState = (ParState) info;
 
         if (parLocalGame.canMove(playerNumber)) {
-            if (parState.getCurrentSubstage() == parState.Roll) {
-                rollAction = new ParRollAction(this);
-                sleep(1000);
-                game.sendAction(rollAction);
-            }
-            if (parState.getCurrentSubstage() == parState.Begin_Move || parState.getCurrentSubstage() == parState.Mid_Move) {
-                for (int i = 0; i < 4; i++) {
-                    player0LocationsX[i] = parState.getPawnLocationsXForPlayer(0, i);
-                    player1LocationsX[i] = parState.getPawnLocationsXForPlayer(1, i);
-                    player2LocationsX[i] = parState.getPawnLocationsXForPlayer(2, i);
-                    player3LocationsX[i] = parState.getPawnLocationsXForPlayer(3, i);
-                    player0LocationsY[i] = parState.getPawnLocationsYForPlayer(0, i);
-                    player1LocationsY[i] = parState.getPawnLocationsYForPlayer(1, i);
-                    player2LocationsY[i] = parState.getPawnLocationsYForPlayer(2, i);
-                    player3LocationsY[i] = parState.getPawnLocationsYForPlayer(3, i);
+            if (parState.getCheckLegalMoveActionMade() == true) {
+                if (parState.getCurrentSubstage() == parState.Roll) {
+                    rollAction = new ParRollAction(this);
+                    sleep(4000);
+                    game.sendAction(rollAction);
+                    sleep(2000);
                 }
-                // get the rectangle of all of the pawns
-                // if the rectangle is -1... then it is in the starting locations
-                for (int j = 0; j < 4; j++) {
-                    player0Rect[j] = parState.getRect(player0LocationsX[j], player0LocationsY[j]);
-                    player1Rect[j] = parState.getRect(player1LocationsX[j], player1LocationsY[j]);
-                    player2Rect[j] = parState.getRect(player2LocationsX[j], player2LocationsY[j]);
-                    player3Rect[j] = parState.getRect(player3LocationsX[j], player3LocationsY[j]);
-                }
-                for (int a = 0; a < 4; a++) {
+                if (parState.getCurrentSubstage() == parState.Begin_Move || parState.getCurrentSubstage() == parState.Mid_Move) {
 
-                    switch (playerNumber) {
-                        case 0:
-                            if ((parState.getLegalMoves("dieVal1", a) > parState.getLegalMoves("dieVal2", a)) && (parState.getLegalMoves("dieVal1", a) > parState.getLegalMoves("totalDieVal", a))) {
-                                useDieAction = new ParUseDieAction(this, ((ParState) info).getDice1Val(), 0);
-                                game.sendAction(selectAction);
-                                break;
-                            } else if ((parState.getLegalMoves("dieVal2", a) > parState.getLegalMoves("dieVal1", a)) && (parState.getLegalMoves("dieVal2", a) > parState.getLegalMoves("totalDieVal", a))) {
-                                useDieAction = new ParUseDieAction(this, ((ParState) info).getDice2Val(), 0);
-                                game.sendAction(selectAction);
-                                break;
-                            } else if ((parState.getLegalMoves("totalDieVal", a) > parState.getLegalMoves("dieVal1", a)) && (parState.getLegalMoves("totalDieVal", a) > parState.getLegalMoves("dieVal2", a))) {
-                                useDieAction = new ParUseDieAction(this, ((ParState) info).getDice1Val() + ((ParState) info).getDice2Val(), 0);
-                                game.sendAction(selectAction);
-                                break;
+                    for (int i = 0; i < 4; i++) {
+                        player0LocationsX[i] = parState.getPawnLocationsXForPlayer(0, i);
+                        player1LocationsX[i] = parState.getPawnLocationsXForPlayer(1, i);
+                        player2LocationsX[i] = parState.getPawnLocationsXForPlayer(2, i);
+                        player3LocationsX[i] = parState.getPawnLocationsXForPlayer(3, i);
+                        player0LocationsY[i] = parState.getPawnLocationsYForPlayer(0, i);
+                        player1LocationsY[i] = parState.getPawnLocationsYForPlayer(1, i);
+                        player2LocationsY[i] = parState.getPawnLocationsYForPlayer(2, i);
+                        player3LocationsY[i] = parState.getPawnLocationsYForPlayer(3, i);
+                    }
+                    // get the rectangle of all of the pawns
+                    // if the rectangle is -1... then it is in the starting locations
+                    for (int j = 0; j < 4; j++) {
+                        player0Rect[j] = parState.getRect(player0LocationsX[j], player0LocationsY[j]);
+                        player1Rect[j] = parState.getRect(player1LocationsX[j], player1LocationsY[j]);
+                        player2Rect[j] = parState.getRect(player2LocationsX[j], player2LocationsY[j]);
+                        player3Rect[j] = parState.getRect(player3LocationsX[j], player3LocationsY[j]);
+                    }
+
+                    // ToDo: select the pawns with a more thoughtful method
+                    if (parState.getPawnActionMade() == false) {
+                        Log.i("selecting pawn", "" + parState.getPawnActionMade());
+                        Random rand = new Random();
+                        // choose a random pawn
+                        int randSelect = rand.nextInt(4);
+                        selectAction = new ParSelectAction(this, randSelect);
+                        game.sendAction(selectAction);
+                        return;
+                    }
+
+                    else if (parState.getUseDieActionMade() == false) {
+                        for (int a = 0; a < 4; a++) {
+
+                            // ToDo: check if you can move out of the starting location...
+                            // ToDo: check if you can move into the safezone...
+                            // ToDo: check if you can eat another pawn...
+                            // ToDo: check if you can move into the homebase...
+
+                            switch (playerNumber) {
+                                case 0:
+                                    if ((parState.getLegalMoves("dieVal1", a) > parState.getLegalMoves("dieVal2", a)) && (parState.getLegalMoves("dieVal1", a) > parState.getLegalMoves("totalDieVal", a))) {
+                                        useDieAction = new ParUseDieAction(this, ((ParState) info).getDice1Val(), 0);
+                                        game.sendAction(selectAction);
+                                        return;
+                                    } else if ((parState.getLegalMoves("dieVal2", a) > parState.getLegalMoves("dieVal1", a)) && (parState.getLegalMoves("dieVal2", a) > parState.getLegalMoves("totalDieVal", a))) {
+                                        useDieAction = new ParUseDieAction(this, ((ParState) info).getDice2Val(), 0);
+                                        game.sendAction(selectAction);
+                                        return;
+                                    } else if ((parState.getLegalMoves("totalDieVal", a) > parState.getLegalMoves("dieVal1", a)) && (parState.getLegalMoves("totalDieVal", a) > parState.getLegalMoves("dieVal2", a))) {
+                                        useDieAction = new ParUseDieAction(this, ((ParState) info).getDice1Val() + ((ParState) info).getDice2Val(), 0);
+                                        game.sendAction(selectAction);
+                                        return;
+                                    }
+
+                                case 1:
+                                    if ((parState.getLegalMoves("dieVal1", a) > parState.getLegalMoves("dieVal2", a)) && (parState.getLegalMoves("dieVal1", a) > parState.getLegalMoves("totalDieVal", a))) {
+                                        useDieAction = new ParUseDieAction(this, ((ParState) info).getDice1Val(), 0);
+                                        game.sendAction(selectAction);
+                                        return;
+                                    } else if ((parState.getLegalMoves("dieVal2", a) > parState.getLegalMoves("dieVal1", a)) && (parState.getLegalMoves("dieVal2", a) > parState.getLegalMoves("totalDieVal", a))) {
+                                        useDieAction = new ParUseDieAction(this, ((ParState) info).getDice2Val(), 0);
+                                        game.sendAction(selectAction);
+                                        return;
+                                    } else if ((parState.getLegalMoves("totalDieVal", a) > parState.getLegalMoves("dieVal1", a)) && (parState.getLegalMoves("totalDieVal", a) > parState.getLegalMoves("dieVal2", a))) {
+                                        useDieAction = new ParUseDieAction(this, ((ParState) info).getDice1Val() + ((ParState) info).getDice2Val(), 0);
+                                        game.sendAction(selectAction);
+                                        return;
+                                    }
+
+                                case 2:
+                                    if ((parState.getLegalMoves("dieVal1", a) > parState.getLegalMoves("dieVal2", a)) && (parState.getLegalMoves("dieVal1", a) > parState.getLegalMoves("totalDieVal", a))) {
+                                        useDieAction = new ParUseDieAction(this, ((ParState) info).getDice1Val(), 0);
+                                        game.sendAction(selectAction);
+                                        return;
+                                    } else if ((parState.getLegalMoves("dieVal2", a) > parState.getLegalMoves("dieVal1", a)) && (parState.getLegalMoves("dieVal2", a) > parState.getLegalMoves("totalDieVal", a))) {
+                                        useDieAction = new ParUseDieAction(this, ((ParState) info).getDice2Val(), 0);
+                                        game.sendAction(selectAction);
+                                        return;
+                                    } else if ((parState.getLegalMoves("totalDieVal", a) > parState.getLegalMoves("dieVal1", a)) && (parState.getLegalMoves("totalDieVal", a) > parState.getLegalMoves("dieVal2", a))) {
+                                        useDieAction = new ParUseDieAction(this, ((ParState) info).getDice1Val() + ((ParState) info).getDice2Val(), 0);
+                                        game.sendAction(selectAction);
+                                        return;
+                                    }
+
+
+                                case 3:
+                                    if ((parState.getLegalMoves("dieVal1", a) > parState.getLegalMoves("dieVal2", a)) && (parState.getLegalMoves("dieVal1", a) > parState.getLegalMoves("totalDieVal", a))) {
+                                        useDieAction = new ParUseDieAction(this, ((ParState) info).getDice1Val(), 0);
+                                        game.sendAction(selectAction);
+                                        return;
+                                    } else if ((parState.getLegalMoves("dieVal2", a) > parState.getLegalMoves("dieVal1", a)) && (parState.getLegalMoves("dieVal2", a) > parState.getLegalMoves("totalDieVal", a))) {
+                                        useDieAction = new ParUseDieAction(this, ((ParState) info).getDice2Val(), 0);
+                                        game.sendAction(selectAction);
+                                        return;
+                                    } else if ((parState.getLegalMoves("totalDieVal", a) > parState.getLegalMoves("dieVal1", a)) && (parState.getLegalMoves("totalDieVal", a) > parState.getLegalMoves("dieVal2", a))) {
+                                        useDieAction = new ParUseDieAction(this, ((ParState) info).getDice1Val() + ((ParState) info).getDice2Val(), 0);
+                                        game.sendAction(selectAction);
+                                        return;
+                                    }
+
+
                             }
-
-                        case 1:
-                            if ((parState.getLegalMoves("dieVal1", a) > parState.getLegalMoves("dieVal2", a)) && (parState.getLegalMoves("dieVal1", a) > parState.getLegalMoves("totalDieVal", a))) {
-                                useDieAction = new ParUseDieAction(this, ((ParState) info).getDice1Val(), 0);
-                                game.sendAction(selectAction);
-                                break;
-                            } else if ((parState.getLegalMoves("dieVal2", a) > parState.getLegalMoves("dieVal1", a)) && (parState.getLegalMoves("dieVal2", a) > parState.getLegalMoves("totalDieVal", a))) {
-                                useDieAction = new ParUseDieAction(this, ((ParState) info).getDice2Val(), 0);
-                                game.sendAction(selectAction);
-                                break;
-                            } else if ((parState.getLegalMoves("totalDieVal", a) > parState.getLegalMoves("dieVal1", a)) && (parState.getLegalMoves("totalDieVal", a) > parState.getLegalMoves("dieVal2", a))) {
-                                useDieAction = new ParUseDieAction(this, ((ParState) info).getDice1Val() + ((ParState) info).getDice2Val(), 0);
-                                game.sendAction(selectAction);
-                                break;
-                            }
-
-                        case 2:
-                            if ((parState.getLegalMoves("dieVal1", a) > parState.getLegalMoves("dieVal2", a)) && (parState.getLegalMoves("dieVal1", a) > parState.getLegalMoves("totalDieVal", a))) {
-                                useDieAction = new ParUseDieAction(this, ((ParState) info).getDice1Val(), 0);
-                                game.sendAction(selectAction);
-                                break;
-                            } else if ((parState.getLegalMoves("dieVal2", a) > parState.getLegalMoves("dieVal1", a)) && (parState.getLegalMoves("dieVal2", a) > parState.getLegalMoves("totalDieVal", a))) {
-                                useDieAction = new ParUseDieAction(this, ((ParState) info).getDice2Val(), 0);
-                                game.sendAction(selectAction);
-                                break;
-                            } else if ((parState.getLegalMoves("totalDieVal", a) > parState.getLegalMoves("dieVal1", a)) && (parState.getLegalMoves("totalDieVal", a) > parState.getLegalMoves("dieVal2", a))) {
-                                useDieAction = new ParUseDieAction(this, ((ParState) info).getDice1Val() + ((ParState) info).getDice2Val(), 0);
-                                game.sendAction(selectAction);
-                                break;
-                            }
-
-
-                        case 3:
-                            if ((parState.getLegalMoves("dieVal1", a) > parState.getLegalMoves("dieVal2", a)) && (parState.getLegalMoves("dieVal1", a) > parState.getLegalMoves("totalDieVal", a))) {
-                                useDieAction = new ParUseDieAction(this, ((ParState) info).getDice1Val(), 0);
-                                game.sendAction(selectAction);
-                                break;
-                            } else if ((parState.getLegalMoves("dieVal2", a) > parState.getLegalMoves("dieVal1", a)) && (parState.getLegalMoves("dieVal2", a) > parState.getLegalMoves("totalDieVal", a))) {
-                                useDieAction = new ParUseDieAction(this, ((ParState) info).getDice2Val(), 0);
-                                game.sendAction(selectAction);
-                                break;
-                            } else if ((parState.getLegalMoves("totalDieVal", a) > parState.getLegalMoves("dieVal1", a)) && (parState.getLegalMoves("totalDieVal", a) > parState.getLegalMoves("dieVal2", a))) {
-                                useDieAction = new ParUseDieAction(this, ((ParState) info).getDice1Val() + ((ParState) info).getDice2Val(), 0);
-                                game.sendAction(selectAction);
-                                break;
-                            }
-
-
+                        }
+                    }
+                    else {
+                        moveAction = new ParMoveAction(this);
+                        sleep(300);
+                        game.sendAction(moveAction);
+                        return;
                     }
                 }
-                moveAction = new ParMoveAction(this);
-                sleep(300);
-                game.sendAction(moveAction);
+            }
+            else {
+                checkLegalMoveAction = new ParCheckLegalMoveAction(this);
+                game.sendAction(checkLegalMoveAction);
+                return;
             }
         }
     }
