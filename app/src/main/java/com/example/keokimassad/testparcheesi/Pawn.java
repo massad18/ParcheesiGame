9ba5dde8045918603c54261272c62936de5fa7da
bhattara18 <bhattara18@up.edu>
2016-12-09 22:38:59 -0800

    Updated formatting of Pawn class.

--- app/src/main/java/com/example/keokimassad/testparcheesi/Pawn.java
+++ app/src/main/java/com/example/keokimassad/testparcheesi/Pawn.java
@@ -7,44 +7,53 @@ import android.graphics.Paint;
 import java.io.Serializable;
 
 /**
+ * Class Pawn
+ *
+ * A class to draw pawns when called. Depending on if it is called when a pawn is selected by the
+ * human player GUI, will draw an outline
+ *
  * Created by AvayaBhattarai on 11/20/16.
+ * @author Avaya Bhattarai
+ * @version November 2016
+ *
  */
 
 public class Pawn implements Serializable {
 
     private static final long serialVersionUID = 3443195316586596539L;
 
-    public float xCor;
-    public float yCor;
-    public float size = 10;
+    //instance variables
+
+    public float xCor; //x coordinate
+    public float yCor; //y coordinate
+    public float size = 10; //default size of the pawns
     public Paint paint = new Paint();
-    public boolean safe = false;
-    public float selectedSize;
+    public float selectedSize; //size when selected.
 
-
+    //Constructor for the Pawn class
     public Pawn(float xCor, float yCor) {
         this.xCor = xCor;
         this.yCor = yCor;
     }
 
-
+    //Draw method to draw a pawn on the board, will draw both selected pawns and non selected pawns
     public void drawOn(float xCor, float yCor, Canvas canvas, int playerIndex, boolean selected)
     {
-        if(playerIndex == 0)
+        if(playerIndex == 0) //Red player
         {
             if (selected) {
-                paint.setColor(Color.LTGRAY);
-                selectedSize = size-5;
+                paint.setColor(Color.LTGRAY); //outline of the pawn
+                selectedSize = size-5; //inner color of pawn
             }
             else {
                 paint.setColor(Color.BLACK);
                 selectedSize = size-2;
             }
-            canvas.drawCircle(xCor, yCor, size, paint);
-            paint.setColor(0xFF9F1515);
-            canvas.drawCircle(xCor, yCor, selectedSize, paint);
+            canvas.drawCircle(xCor, yCor, size, paint); //draws outline
+            paint.setColor(0xFF9F1515); //sets color to dark red
+            canvas.drawCircle(xCor, yCor, selectedSize, paint); //draws inner color
         }
-        else if(playerIndex == 1)
+        else if(playerIndex == 1) //Blue player
         {
             if (selected) {
                 paint.setColor(Color.LTGRAY);
@@ -56,7 +65,7 @@ public class Pawn implements Serializable {
             paint.setColor(Color.BLUE);
             canvas.drawCircle(xCor, yCor, size-2, paint);
         }
-        else if(playerIndex == 2)
+        else if(playerIndex == 2) //Yellow Player
         {
             if (selected) {
                 paint.setColor(Color.LTGRAY);
@@ -65,10 +74,10 @@ public class Pawn implements Serializable {
                 paint.setColor(Color.BLACK);
             }
             canvas.drawCircle(xCor, yCor, size, paint);
-            paint.setColor(0XFFe5d31d);
+            paint.setColor(0XFFe5d31d); //sets color to darker yellow
             canvas.drawCircle(xCor, yCor, size-2, paint);
         }
-        else
+        else //Green Player
         {
             if (selected) {
                 paint.setColor(Color.LTGRAY);
@@ -81,16 +90,6 @@ public class Pawn implements Serializable {
             canvas.drawCircle(xCor, yCor, size-2, paint);
         }
 
-    }
-
-    public boolean isSafe()
-    {
-        return safe;
-    }
-
-    public void setSafe(boolean safetyChange)
-    {
-        safe = safetyChange;
     }
 
 }
